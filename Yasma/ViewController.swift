2acfc1c72df6985e5691afeb8c829f2c7ff91ada
sabapathy7 <trksabapathy7@gmail.com>
2018-08-07 12:58:44 +0530

    Initial Commit

---
+++ Yasma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yasma
+//
+//  Created by kanagasabapathy on 07/08/18.
+//  Copyright © 2018 kanagasabapathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
